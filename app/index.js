c9aea6598cd72e157fcb5e8bb3415a155abd344c
malha3 <contactmalha@gmail.com>
2020-07-07 14:56:59 +0200

    #4

--- app/index.js
+++ app/index.js
@@ -2,12 +2,24 @@
 let express = require('express'),
     app = express();
 
+// Gestion Fichiers Systèmes
+let fs = require('fs'),
+path = require('path');
 
 // --- middleware
 // - body-parser needed to catch and to treat information inside req.body
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
+// Gestion des pages front
+let helpers 	= require('view-helpers'),
+	consolidate = require('consolidate');
+
+app.engine('html', consolidate['mustache']);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/front');
+
 
 // ------------------------
 // ROUTES RESOURCES
@@ -40,6 +52,15 @@ app.put('/salades/:idSalade',(req, res)=>{
 app.delete('/salades/:idSalade',(req, res)=>{
     res.status(204).json()
 })
+
+// ------------------------
+// ROUTES VUES
+// ------------------------
+app.get('/',(req,res)=>{
+    res.render('index',{message : req.query.message})
+});
+
+
 
 // ------------------------
 // START SERVER
